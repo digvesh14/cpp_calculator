05b056772c05c47cf50f5e3566bb791962f51d86
digvesh14 <145886370+digvesh14@users.noreply.github.com>
2023-09-24 09:59:54 +0530

    practice

---
+++ stringaddition.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name="digvesh";
+    string surname="bhatkar";
+
+    string fullname=name+" "+surname;
+    cout << fullname << "\n";
+    name="jyotsna";
+    cout << name;
+
+cout << "\n";
+cout << " enter your first name :";
+cin >> name;
+cout << "\n your name is " << name;
+    return 0;
+}
